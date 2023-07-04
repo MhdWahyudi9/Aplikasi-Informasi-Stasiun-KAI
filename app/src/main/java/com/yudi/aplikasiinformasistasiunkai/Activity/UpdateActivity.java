d6a9fa934c3e5f80bc0bde22969b9fd63249c63c
MhdWahyudi9 <mhd.wahyudi004@mhs.mdp.ac.id>
2023-07-04 15:20:39 +0700

    Make Update & Delete Dialog Feature

--- app/src/main/java/com/yudi/aplikasiinformasistasiunkai/Activity/UpdateActivity.java
+++ app/src/main/java/com/yudi/aplikasiinformasistasiunkai/Activity/UpdateActivity.java
@@ -1,16 +1,112 @@
 package com.yudi.aplikasiinformasistasiunkai.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.yudi.aplikasiinformasistasiunkai.API.ApiRequest;
+import com.yudi.aplikasiinformasistasiunkai.API.RetroServer;
+import com.yudi.aplikasiinformasistasiunkai.Model.StasiunKAIResponse;
 import com.yudi.aplikasiinformasistasiunkai.R;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class UpdateActivity extends AppCompatActivity {
+
+    private ProgressBar pbLoading;
+    private EditText etNama, etLinkFoto, etKota, etLuas, etSejarah;
+    private String nama, linkFoto, kota, sejarah;
+    private Integer luas;
+
+    private String yId, yNama, yLinkFoto, yKota, ySejarah, yLuas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
+
+        pbLoading = findViewById(R.id.pbLoading);
+
+        etNama = findViewById(R.id.etNama);
+        etLinkFoto = findViewById(R.id.etLinkFoto);
+        etKota = findViewById(R.id.etKota);
+        etLuas = findViewById(R.id.etLuas);
+        etSejarah = findViewById(R.id.etSejarah);
+
+        Button btnUpdate = findViewById(R.id.btnUpdate);
+
+        Intent getData = getIntent();
+        yId = getData.getStringExtra("xId");
+        yNama = getData.getStringExtra("xNama");
+        yKota = getData.getStringExtra("xKota");
+        yLuas = getData.getStringExtra("xLuas");
+        ySejarah = getData.getStringExtra("xSejarah");
+        yLinkFoto = getData.getStringExtra("xLinkFoto");
+
+        etNama.setText(yNama);
+        etKota.setText(yKota);
+        etLinkFoto.setText(yLinkFoto);
+        etSejarah.setText(ySejarah);
+        etLuas.setText(yLuas);
+
+        btnUpdate.setOnClickListener(view -> {
+            nama = etNama.getText().toString();
+            linkFoto = etLinkFoto.getText().toString();
+            kota = etKota.getText().toString();
+            luas = Integer.valueOf(etLuas.getText().toString());
+            sejarah = etSejarah.getText().toString();
+
+            if (nama.trim().equals("")) {
+                etNama.setError("Field nama harus diisi !");
+            }
+            else if (linkFoto.trim().equals("")) {
+                etLinkFoto.setError("Field Link Foto harus diisi !");
+            }
+            else if (kota.trim().equals("")) {
+                etKota.setError("Field Kota harus diisi !");
+            }
+            else if (luas.toString().equals("")) {
+                etLuas.setError("Field Luas harus diisi !");
+            }
+            else if (sejarah.trim().equals("")) {
+                etSejarah.setError("Field Sejarah harus diisi !");
+            }
+            else {
+                updateStasiunKai();
+            }
+        });
+    }
+
+    private void updateStasiunKai() {
+        pbLoading.setVisibility(View.VISIBLE);
+        ApiRequest Api = RetroServer.connectRetrofit().create(ApiRequest.class);
+        Call<StasiunKAIResponse> updateData = Api.stasiunkasiUpdate(Integer.valueOf(yId), nama, linkFoto, sejarah, kota, luas);
+
+        updateData.enqueue(new Callback<StasiunKAIResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<StasiunKAIResponse> call, @NonNull Response<StasiunKAIResponse> response) {
+                pbLoading.setVisibility(View.GONE);
+                if (response.body() != null) {
+                    String message = response.body().getMessage();
+                    Toast.makeText(UpdateActivity.this, message, Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<StasiunKAIResponse> call, @NonNull Throwable t) {
+                pbLoading.setVisibility(View.GONE);
+                Toast.makeText(UpdateActivity.this, "Gagal menghubungi server !", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/yudi/aplikasiinformasistasiunkai/Adapter/StasiunkaiAdapter.java
+++ app/src/main/java/com/yudi/aplikasiinformasistasiunkai/Adapter/StasiunkaiAdapter.java
@@ -2,21 +2,35 @@ package com.yudi.aplikasiinformasistasiunkai.Adapter;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.yudi.aplikasiinformasistasiunkai.API.ApiRequest;
+import com.yudi.aplikasiinformasistasiunkai.API.RetroServer;
+import com.yudi.aplikasiinformasistasiunkai.Activity.AddActivity;
+import com.yudi.aplikasiinformasistasiunkai.Activity.MainActivity;
+import com.yudi.aplikasiinformasistasiunkai.Activity.UpdateActivity;
 import com.yudi.aplikasiinformasistasiunkai.Model.StasiunKAI;
+import com.yudi.aplikasiinformasistasiunkai.Model.StasiunKAIResponse;
 import com.yudi.aplikasiinformasistasiunkai.R;
 import com.yudi.aplikasiinformasistasiunkai.Utils.NumberFormatter;
 
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class StasiunkaiAdapter extends RecyclerView.Adapter<StasiunkaiAdapter.VHStasiunKAI> {
 
@@ -39,6 +53,7 @@ public class StasiunkaiAdapter extends RecyclerView.Adapter<StasiunkaiAdapter.VH
     @Override
     public void onBindViewHolder(@NonNull StasiunkaiAdapter.VHStasiunKAI holder, int position) {
         StasiunKAI stasiunKAI = listStasiunKAI.get(position);
+        holder.tvId.setText(stasiunKAI.getId().toString());
         holder.tvNama.setText(stasiunKAI.getNama());
         holder.tvKota.setText(stasiunKAI.getKota());
         holder.tvSejarah.setText(stasiunKAI.getSejarah());
@@ -47,6 +62,8 @@ public class StasiunkaiAdapter extends RecyclerView.Adapter<StasiunkaiAdapter.VH
                 .load(stasiunKAI.getFoto())
                 .placeholder(R.drawable.placeholder_image)
                 .into(holder.ivFoto);
+        holder.linkFoto = stasiunKAI.getFoto();
+        holder.luas = stasiunKAI.getLuas().toString();
     }
 
     @Override
@@ -56,15 +73,65 @@ public class StasiunkaiAdapter extends RecyclerView.Adapter<StasiunkaiAdapter.VH
 
     public class VHStasiunKAI extends RecyclerView.ViewHolder {
         ImageView ivFoto;
-        TextView tvNama, tvSejarah, tvKota, tvLuas;
+        TextView tvId, tvNama, tvSejarah, tvKota, tvLuas;
+
+        String linkFoto;
+
+        String luas;
         public VHStasiunKAI(@NonNull View itemView) {
             super(itemView);
 
             ivFoto = itemView.findViewById(R.id.ivFoto);
+            tvId = itemView.findViewById(R.id.tvId);
             tvNama = itemView.findViewById(R.id.tvNama);
             tvKota = itemView.findViewById(R.id.tvKota);
             tvLuas = itemView.findViewById(R.id.tvLuas);
             tvSejarah = itemView.findViewById(R.id.tvSejarah);
+
+            itemView.setOnLongClickListener(view -> {
+                AlertDialog.Builder message = new AlertDialog.Builder(ctx);
+                message.setTitle("Pehatian");
+                message.setMessage("Anda memilih  " + tvNama.getText().toString() + ".\nOperasi apa yang ingin dilakukan ?");
+
+                message.setPositiveButton("Ubah Data", (dialogInterface, i) -> {
+                    Intent sendData = new Intent(ctx, UpdateActivity.class);
+                    sendData.putExtra("xId", tvId.getText().toString());
+                    sendData.putExtra("xNama", tvNama.getText().toString());
+                    sendData.putExtra("xKota", tvKota.getText().toString());
+                    sendData.putExtra("xLuas", luas);
+                    sendData.putExtra("xSejarah", tvSejarah.getText().toString());
+                    sendData.putExtra("xLinkFoto", linkFoto);
+                    ctx.startActivity(sendData);
+                });
+
+                message.setNegativeButton("Hapus Data", (dialogInterface, i) -> {
+                    deleteData(Integer.valueOf(tvId.getText().toString()));
+                });
+
+                message.show();
+                return false;
+            });
+        }
+
+        void deleteData(Integer id) {
+            ApiRequest Api = RetroServer.connectRetrofit().create(ApiRequest.class);
+            Call<StasiunKAIResponse> deleteData = Api.stasiunkasiDelete(id);
+
+            deleteData.enqueue(new Callback<StasiunKAIResponse>() {
+                @Override
+                public void onResponse(@NonNull Call<StasiunKAIResponse> call, @NonNull Response<StasiunKAIResponse> response) {
+                    if (response.body() != null) {
+                        String message = response.body().getMessage();
+                        Toast.makeText(ctx, message, Toast.LENGTH_SHORT).show();
+                        ((MainActivity) ctx).readData();
+                    }
+                }
+
+                @Override
+                public void onFailure(@NonNull Call<StasiunKAIResponse> call, @NonNull Throwable t) {
+                    Toast.makeText(ctx, "Gagal menghubungi server !", Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 }
